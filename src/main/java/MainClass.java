5ef1a64b43df0413099111f26de17e566290d861
ShabanAntony <shabanantony@gmail.com>
2020-07-20 01:56:51 +0300

    add driversCalls

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -19,73 +19,14 @@ public class MainClass {
 
         driver.manage().window().maximize();
 
-//        driver.get("https://en.wikipedia.org/wiki/Main_Page");
-//
-//        driver.findElement(By.xpath("//form//input[@id='searchInput']")).sendKeys("Selenium webDriver");
-//
-//        driver.findElement(By.xpath("//form//input[@id='searchButton']")).click();
-//
-//        System.out.println(driver.findElement(By.xpath("//div[@id='searchText']//input[@id='ooui-php-1']")).getAttribute("value"));
-//
-//        driver.findElement(By.xpath("//div[@id='searchText']//input[@id='ooui-php-1']")).clear();
+        driver.get("https://en.wikipedia.org/wiki/Main_Page");
 
-        driver.get("https://github.com/");
-        driver.findElement(By.xpath("//input[@id='user[login]']")).sendKeys("testUser");
-        driver.findElement(By.xpath("//input[@id='user[email]']")).sendKeys("testUser@email.java");
-        driver.findElement(By.xpath("//input[@id='user[password]']")).sendKeys("233228");
-        driver.findElement(By.xpath("//button[@class='btn-mktg btn-primary-mktg btn-large-mktg f4 btn-block my-3']")).click();
+        WebElement link = driver.findElement(By.xpath("//li[@id='n-aboutsite']/a"));
 
-        String massege = driver.findElement(By.xpath("//dd[@class='error']")).getText();
+        System.out.println(link.getText());
 
-        if (massege.contains("not available")){
-            System.out.println("Error!");
-        }else
-        System.out.println("Log in passed");
+        link.click();
 
-//        driver.findElement(By.linkText("Log in"));
-//
-//        WebElement link = driver.findElement(By.linkText("Log in"));
-//
-//        WebElement link2 = driver.findElement(By.partialLinkText("Donate"));
-//
-//        WebElement search = driver.findElement(By.name("search"));
-//
-//        WebElement buttonSearch = driver.findElement(By.className("searchButton"));
-//
-//        WebElement buttonSearchMw = driver.findElement(By.id("mw-searchButton"));
-//
-//        WebElement searchCssSelecror = driver.findElement(By.cssSelector("div#p-search"));
-//
-//        System.out.println(link.getText());
-//
-//        System.out.println(link2.getText());
-
-//        driver.get("https://github.com/");
-//
-//        WebElement button = driver.findElement(By.xpath("//form//button[@class='btn-mktg btn-primary-mktg btn-large-mktg f4 btn-block my-3']"));
-//
-//        if (button.getText().equals("Sign up for GitHub")) {
-//            System.out.println("Success!");
-//        }else System.out.println("Fail!");
-//
-//        System.out.println("Button text is: " + button.getText());
-
-
-//        button.submit();
-
-
-
-
-//        driver.navigate().to("https://www.selenium.dev/");
-//
-//        driver.navigate().back();
-//
-//        driver.navigate().forward();
-//
-//
-//        driver.navigate().refresh();
-//
-//        System.out.println(driver.getTitle());
 
       //  driver.quit();
     }
---
+++ src/test/java/driversCalls.java
@@ -0,0 +1,68 @@
+public class driversCalls {
+    //        driver.findElement(By.linkText("Log in"));
+//
+//        WebElement link = driver.findElement(By.linkText("Log in"));
+//
+//        WebElement link2 = driver.findElement(By.partialLinkText("Donate"));
+//
+//        WebElement search = driver.findElement(By.name("search"));
+//
+//        WebElement buttonSearch = driver.findElement(By.className("searchButton"));
+//
+//        WebElement buttonSearchMw = driver.findElement(By.id("mw-searchButton"));
+//
+//        WebElement searchCssSelecror = driver.findElement(By.cssSelector("div#p-search"));
+//
+//        System.out.println(link.getText());
+//
+//        System.out.println(link2.getText());
+
+//        driver.get("https://github.com/");
+//
+//        WebElement button = driver.findElement(By.xpath("//form//button[@class='btn-mktg btn-primary-mktg btn-large-mktg f4 btn-block my-3']"));
+//
+//        if (button.getText().equals("Sign up for GitHub")) {
+//            System.out.println("Success!");
+//        }else System.out.println("Fail!");
+//
+//        System.out.println("Button text is: " + button.getText());
+
+
+//        button.submit();
+
+
+
+
+//        driver.navigate().to("https://www.selenium.dev/");
+//
+//        driver.navigate().back();
+//
+//        driver.navigate().forward();
+//
+//
+//        driver.navigate().refresh();
+//
+//        System.out.println(driver.getTitle());
+
+//        driver.findElement(By.xpath("//form//input[@id='searchInput']")).sendKeys("Selenium webDriver");
+//
+//        driver.findElement(By.xpath("//form//input[@id='searchButton']")).click();
+//
+//        System.out.println(driver.findElement(By.xpath("//div[@id='searchText']//input[@id='ooui-php-1']")).getAttribute("value"));
+//
+//        driver.findElement(By.xpath("//div[@id='searchText']//input[@id='ooui-php-1']")).clear();
+
+//        driver.get("https://github.com/");
+//        driver.findElement(By.xpath("//input[@id='user[login]']")).sendKeys("testUser");
+//        driver.findElement(By.xpath("//input[@id='user[email]']")).sendKeys("testUser@email.java");
+//        driver.findElement(By.xpath("//input[@id='user[password]']")).sendKeys("233228");
+//        driver.findElement(By.xpath("//button[@class='btn-mktg btn-primary-mktg btn-large-mktg f4 btn-block my-3']")).click();
+//
+//        String massege = driver.findElement(By.xpath("//dd[@class='error']")).getText();
+//
+//        if (massege.contains("not available")){
+//            System.out.println("Error!");
+//        }else
+//        System.out.println("Log in passed");
+
+}
